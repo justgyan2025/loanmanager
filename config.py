8d606a1dea030cf4a33ce4b0fb0ae00a4d3519a5
testing <justgyan2025@gmail.com>
2025-02-14 01:39:34 +0530

    Initial commit

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key-here'
-    SQLALCHEMY_DATABASE_URI = 'postgresql://shiv:bKbEgU8Gs3KdqnaqSwnXfPl3W2fv3dk2@dpg-cun38m56l47c739b6m0g-a/loanmangerdb'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SECRET_KEY = os.getenv("SECRET_KEY", "123456789")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://shiv:bKbEgU8Gs3KdqnaqSwnXfPl3W2fv3dk2@dpg-cun38m56l47c739b6m0g-a/loanmangerdb")
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
