c8359b16da7dff8bbb8dd74c6b1ceda1d52b8083
testing <justgyan2025@gmail.com>
2025-02-14 02:35:35 +0530

    Initial commit

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import os
+from dotenv import load_dotenv
 from datetime import timedelta
 
+
+# Load environment variables
+load_dotenv()
+
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY", "123456789")
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "postgresql://shiv:bKbEgU8Gs3KdqnaqSwnXfPl3W2fv3dk2@dpg-cun38m56l47c739b6m0g-a/loanmangerdb")
+    SECRET_KEY = os.getenv("SECRET_KEY", "default-secret-key")  # Fallback if .env is missing
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")  # PostgreSQL connection URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    
